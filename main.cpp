efe0781c6ce4c776dbd6a68576dd4030d85e6046
abobadev225 <vaseninnazari@gmail.com>
2024-11-06 21:21:55 +0200

    исправил поиск
    2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <windows.h>
+#include <algorithm>
+#include <cctype>
 using namespace std;
 
 struct Book {
@@ -11,22 +13,20 @@ struct Book {
     int id;
 };
 
+void toLowerCase(string& str) {
+    transform(str.begin(), str.end(), str.begin(), ::tolower);
+}
+
 void addBook(Book*& library, int& size, const Book& newBook) {
-
     Book* newLibrary = new Book[size + 1];
-
     for (int i = 0; i < size; i++) {
         newLibrary[i] = library[i];
     }
-
     newLibrary[size] = newBook;
-
     delete[] library;
-
     library = newLibrary;
     size++;
 }
-
 
 void removeBook(Book*& library, int& size, int bookId) {
     int index = -1;
@@ -38,97 +38,107 @@ void removeBook(Book*& library, int& size, int bookId) {
     }
 
     if (index == -1) {
-        cout << "Книгу з ідентифікатором " << bookId << " не знайдено." << endl;
+        cout << "Book with ID " << bookId << " not found." << endl;
         return;
     }
 
     Book* newLibrary = new Book[size - 1];
-
     for (int i = 0, j = 0; i < size; i++) {
         if (i != index) {
             newLibrary[j] = library[i];
             j++;
         }
     }
-
-
     delete[] library;
-
     library = newLibrary;
     size--;
 }
 
-
 void displayBooks(const Book* library, int size) {
     for (int i = 0; i < size; i++) {
-        cout << "Ідентифікатор: " << library[i].id << endl;
-        cout << "Назва: " << library[i].title << endl;
-        cout << "Автор: " << library[i].author << endl;
-        cout << "Рік видання: " << library[i].year << endl;
-        cout << "Жанр: " << library[i].genre << endl;
+        cout << "ID: " << library[i].id << endl;
+        cout << "Title: " << library[i].title << endl;
+        cout << "Author: " << library[i].author << endl;
+        cout << "Year: " << library[i].year << endl;
+        cout << "Genre: " << library[i].genre << endl;
         cout << "------------------------" << endl;
     }
 }
+
 void findBook(Book* library, int size, const string& find) {
     bool found = false;
+    string findLower = find;
+    toLowerCase(findLower);
+
     for (int i = 0; i < size; i++) {
-        if (find == library[i].author) {
-            cout << "Знайдена книга від цього автора!" << endl;
-            cout << "Назва: " << library[i].title << endl;
-            cout << "Автор: " << library[i].author << endl;
-            cout << "Рік видання: " << library[i].year << endl;
-            cout << "Жанр: " << library[i].genre << endl;
+        string titleLower = library[i].title;
+        string authorLower = library[i].author;
+        string genreLower = library[i].genre;
+
+        toLowerCase(titleLower);
+        toLowerCase(authorLower);
+        toLowerCase(genreLower);
+
+        if (authorLower == findLower) {
+            cout << "Found a book by this author!" << endl;
+            cout << "Title: " << library[i].title << endl;
+            cout << "Author: " << library[i].author << endl;
+            cout << "Year: " << library[i].year << endl;
+            cout << "Genre: " << library[i].genre << endl;
             cout << "------------------------" << endl;
             found = true;
             break;
         }
-        else if (find == library[i].title) {
-            cout << "Знайдена книга з цією назвою!" << endl;
-            cout << "Назва: " << library[i].title << endl;
-            cout << "Автор: " << library[i].author << endl;
-            cout << "Рік видання: " << library[i].year << endl;
-            cout << "Жанр: " << library[i].genre << endl;
+        else if (titleLower == findLower) {
+            cout << "Found a book with this title!" << endl;
+            cout << "Title: " << library[i].title << endl;
+            cout << "Author: " << library[i].author << endl;
+            cout << "Year: " << library[i].year << endl;
+            cout << "Genre: " << library[i].genre << endl;
             cout << "------------------------" << endl;
             found = true;
             break;
         }
-        else if (find == library[i].genre) {
-            cout << "Знайдена книга цього жанру!" << endl;
-            cout << "Назва: " << library[i].title << endl;
-            cout << "Автор: " << library[i].author << endl;
-            cout << "Рік видання: " << library[i].year << endl;
-            cout << "Жанр: " << library[i].genre << endl;
+        else if (genreLower == findLower) {
+            cout << "Found a book of this genre!" << endl;
+            cout << "Title: " << library[i].title << endl;
+            cout << "Author: " << library[i].author << endl;
+            cout << "Year: " << library[i].year << endl;
+            cout << "Genre: " << library[i].genre << endl;
             cout << "------------------------" << endl;
             found = true;
             break;
         }
     }
+
     if (!found) {
-        cout << "Книга не знайдена." << endl;
+        cout << "Book not found." << endl;
     }
 }
+
 int main() {
     system("chcp 65001 > nul");
+
     Book* library = nullptr;
     int size = 0;
     string find;
-    Book book1 = { "Війна і мир", "Лев Толстой", 1869, "Роман", 1 };
-    addBook(library, size, book1);
 
-    Book book2 = { "1984", "Джордж Орвелл", 1949, "Антиутопія", 2 };
+    Book book1 = { "War and Peace", "Leo Tolstoy", 1869, "Novel", 1 };
+    addBook(library, size, book1);
+    Book book2 = { "1984", "George Orwell", 1949, "Dystopia", 2 };
     addBook(library, size, book2);
 
-
-    cout << "Колекція книг:" << endl;
+    cout << "Book Collection:" << endl;
     displayBooks(library, size);
 
-    removeBook(library, size, 1);
+    cout << "Search for a book by title, author, or genre:" << endl;
 
-    cout << "find a book" << endl;
-    cin >> find;
+    getline(cin, find);
+
+    cout << "Entered search string: " << find << endl;
+
     findBook(library, size, find);
-    delete[] library;
 
+    delete[] library;
     return 0;
 }
-
