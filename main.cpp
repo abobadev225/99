f94659cca06999728aea474f0dbbec1467e0230f
abobadev225 <vaseninnazari@gmail.com>
2024-11-10 23:37:09 +0200

    добавил 1:меню для взаемодействия с програмой,2:чтение файла с книгами(и сам файл)
    3

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
+#include <fstream>
 #include <string>
-#include <windows.h>
 #include <algorithm>
-#include <cctype>
 #include <chrono>
 #include <thread>
 using namespace std;
-using namespace std::this_thread;
 using namespace std::chrono;
 struct Book {
     string title;
@@ -16,9 +14,8 @@ struct Book {
     int id;
 };
 
-void toLowerCase(string& str) {
-    transform(str.begin(), str.end(), str.begin(), ::tolower);
-}
+
+
 
 void addBook(Book*& library, int& size, const Book& newBook) {
     Book* newLibrary = new Book[size + 1];
@@ -31,30 +28,38 @@ void addBook(Book*& library, int& size, const Book& newBook) {
     size++;
 }
 
-void removeBook(Book*& library, int& size, int bookId) {
-    int index = -1;
-    for (int i = 0; i < size; i++) {
-        if (library[i].id == bookId) {
-            index = i;
-            break;
-        }
-    }
+void loadBooksFromFile(const string& filename, Book*& library, int& size) {
+    ifstream file(filename);
 
-    if (index == -1) {
-        cout << "Book with ID " << bookId << " not found." << endl;
+    if (!file) {
+        cerr << "file reading error." << endl;
         return;
     }
 
-    Book* newLibrary = new Book[size - 1];
-    for (int i = 0, j = 0; i < size; i++) {
-        if (i != index) {
-            newLibrary[j] = library[i];
-            j++;
-        }
+    while (true) {
+        Book newBook;
+        string yearStr, idStr;
+
+        if (!getline(file, newBook.title)) break;
+        if (!getline(file, newBook.author)) break;
+        if (!getline(file, yearStr)) break;
+        if (!getline(file, newBook.genre)) break;
+        if (!getline(file, idStr)) break;
+
+
+        newBook.year = stoi(yearStr);
+        newBook.id = stoi(idStr);
+
+
+        addBook(library, size, newBook);
+
+
+        string emptyLine;
+        getline(file, emptyLine);
     }
-    delete[] library;
-    library = newLibrary;
-    size--;
+
+    file.close();
+    cout << "Books from file are loaded." << endl;
 }
 
 void displayBooks(const Book* library, int size) {
@@ -67,11 +72,21 @@ void displayBooks(const Book* library, int size) {
         cout << "------------------------" << endl;
     }
 }
-
+void toLowerCase(string& str) {
+    transform(str.begin(), str.end(), str.begin(), ::tolower);
+}
 void findBook(Book* library, int size, const string& find) {
-    bool found = false;
+    int found = 0;
     string findLower = find;
     toLowerCase(findLower);
+    int findId = -1;
+
+
+    try {
+        findId = stoi(findLower);
+    } catch (...) {
+        findId = -1;
+    }
 
     for (int i = 0; i < size; i++) {
         string titleLower = library[i].title;
@@ -89,8 +104,7 @@ void findBook(Book* library, int size, const string& find) {
             cout << "Year: " << library[i].year << endl;
             cout << "Genre: " << library[i].genre << endl;
             cout << "------------------------" << endl;
-            found = true;
-            break;
+            found++;
         }
         else if (titleLower == findLower) {
             cout << "Found a book with this title!" << endl;
@@ -99,8 +113,7 @@ void findBook(Book* library, int size, const string& find) {
             cout << "Year: " << library[i].year << endl;
             cout << "Genre: " << library[i].genre << endl;
             cout << "------------------------" << endl;
-            found = true;
-            break;
+            found++;
         }
         else if (genreLower == findLower) {
             cout << "Found a book of this genre!" << endl;
@@ -109,39 +122,108 @@ void findBook(Book* library, int size, const string& find) {
             cout << "Year: " << library[i].year << endl;
             cout << "Genre: " << library[i].genre << endl;
             cout << "------------------------" << endl;
-            found = true;
-            break;
+            found++;
+        }
+        else if (library[i].id == findId) {
+            cout << "Found a book with this ID!" << endl;
+            cout << "Title: " << library[i].title << endl;
+            cout << "Author: " << library[i].author << endl;
+            cout << "Year: " << library[i].year << endl;
+            cout << "Genre: " << library[i].genre << endl;
+            cout << "------------------------" << endl;
+            found++;
         }
     }
 
-    if (!found) {
+    if (found == 0) {
         cout << "Book not found." << endl;
+    }
+}
+void menu(int& choose, Book*& library, int size) {
+    while (true) {
+        cout << "Choose an option from the menu:" << endl;
+        cout << "1. Display all library books" << endl;
+        cout << "2. Add a book to the library" << endl;
+        cout << "3. Delete a book from the library" << endl;
+        cout << "4. Find a specific book (by author, genre, or ID)" << endl;
+        cout << "5. Exit program" << endl;
+        cout << "------------------------" << endl;
+        cin >> choose;
+
+
+        cin.ignore();
+
+        switch (choose) {
+            case 1:
+                displayBooks(library, size);
+                break;
+            case 2: {
+                Book newBook;
+                cout << "Enter book title: ";
+                getline(cin, newBook.title);
+                cout << "Enter author: ";
+                getline(cin, newBook.author);
+                cout << "Enter year: ";
+                cin >> newBook.year;
+                cin.ignore();
+                cout << "Enter genre: ";
+                getline(cin, newBook.genre);
+                newBook.id = size + 1;
+                addBook(library, size, newBook);
+                break;
+            }
+            case 3: {
+                int deleteId;
+                cout << "Enter the ID of the book to delete: ";
+                cin >> deleteId;
+                cin.ignore();
+                bool found = false;
+                for (int i = 0; i < size; i++) {
+                    if (library[i].id == deleteId) {
+                        found = true;
+                        for (int j = i; j < size - 1; j++) {
+                            library[j] = library[j + 1];
+                        }
+                        size--;
+                        cout << "Book with ID " << deleteId << " deleted successfully!" << endl;
+                        break;
+                    }
+                }
+                if (!found) {
+                    cout << "No book found with this ID." << endl;
+                }
+                break;
+            }
+            case 4: {
+                string search;
+                cout << "Enter author, genre, or ID to search for a book: ";
+                getline(cin, search);
+                findBook(library, size, search);
+                break;
+            }
+            case 5:
+                cout << "Exiting program..." << endl;
+                return;
+            default:
+                cout << "Invalid option. Try again." << endl;
+                break;
+        }
     }
 }
 
 int main() {
-    system("chcp 65001 > nul");
-
     Book* library = nullptr;
+
     int size = 0;
-    string find;
 
-    Book book1 = { "War and Peace", "Leo Tolstoy", 1869, "Novel", 1 };
-    addBook(library, size, book1);
-    Book book2 = { "1984", "George Orwell", 1949, "Dystopia", 2 };
-    addBook(library, size, book2);
+    loadBooksFromFile("lib.txt", library, size);
+    int choose;
 
-    cout << "Book Collection:" << endl;
-    displayBooks(library, size);
+    menu(choose,library,size);
 
-    cout << "Search for a book by title, author, or genre:" << endl;
+    this_thread::sleep_for(seconds(1));
 
-    getline(cin, find);
-
-    cout << "Entered search string: " << find << endl;
-
-    findBook(library, size, find);
-    sleep_for(seconds(4));
     delete[] library;
+
     return 0;
 }
